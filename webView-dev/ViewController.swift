755a6ba82ed7540663676162679e949478377bd7
Dhruvv4 <dhruvvaghela17@gmail.com>
2018-02-15 22:36:51 +0530

    Initial commit

--- webView-dev/ViewController.swift
+++ webView-dev/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textFld: UITextField!
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var backBtn: UIButton!
+    @IBOutlet weak var forwardBtn: UIButton!
+    @IBOutlet weak var reloadBtn: UIButton!
+    
+    let BASE_URL = "https://"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textFld.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
-
-
+    
+    @IBAction func homeBtnWasPressed(_ sender: UIButton) {
+        let url = "www.google.in"
+        let comeplete_url = "\(BASE_URL)\(url)"
+        webView.loadRequest(URLRequest(url: URL(string: comeplete_url)!))
+    }
+    @IBAction func goBtnWasPressed(_ sender: UIButton) {
+        guard let url = textFld.text else { return }
+        let completeUrl = "\(BASE_URL)\(url)"
+        webView.loadRequest(URLRequest(url: URL(string: completeUrl)!))
+        textFld.resignFirstResponder()
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: UIButton) {
+            webView.goBack()
+    }
+    
+    @IBAction func forwardBtnWasPressed(_ sender: UIButton) {
+        webView.goForward()
+    }
+    
+    @IBAction func reloadBtnWasPressed(_ sender: UIButton) {
+        webView.reload()
+    }
 }
+
+
+
+
+
+
+
+
+
 
